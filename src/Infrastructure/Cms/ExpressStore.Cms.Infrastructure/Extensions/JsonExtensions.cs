693970d5ec9f1e2ba86be09ca87c1beff69cc6cd
LesterCerioli <lesterlucasit@hotmail.com>
2022-12-24 15:39:35 -0800

    fet(#20): Building Extensions classes

---
+++ src/Infrastructure/Cms/ExpressStore.Cms.Infrastructure/Extensions/JsonExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace ExpressStore.Cms.Infrastructure.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static string ToJsonString(this object obj, bool camelCase = false, bool indented = false)
+        {
+            var options = new JsonSerializerSettings();
+
+            if (camelCase)
+            {
+                options.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            }
+
+            if (indented)
+            {
+                options.Formatting = Formatting.Indented;
+            }
+
+            return JsonConvert.SerializeObject(obj, options);
+        }
+    }
+}
