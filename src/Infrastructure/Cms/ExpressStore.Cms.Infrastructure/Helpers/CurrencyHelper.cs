78ab7b2af8e12813c9f73f0c80431317d29e6b5e
LesterCerioli <lesterlucasit@hotmail.com>
2022-12-25 05:44:25 -0800

    fet9#18): Building Helpers classes into Cms Infrastructure Project

---
+++ src/Infrastructure/Cms/ExpressStore.Cms.Infrastructure/Helpers/CurrencyHelper.cs
@@ -0,0 +1,13 @@
+namespace ExpressStore.Cms.Infrastructure.Helpers
+{
+    public static class CurrencyHelper
+    {
+        private static readonly List<string> _zeroDecimalCurrencies = new List<string> { "BIF", "DJF", "JPY", "KRW", "PYG", "VND", "XAF", "XPF", "CLP", "GNF", "KMF", "MGA", "RWF", "VUV", "XOF" };
+
+        public static bool IsZeroDecimalCurrencies(CultureInfo cultureInfo)
+        {
+            var regionInfo = new RegionInfo(cultureInfo.LCID);
+            return _zeroDecimalCurrencies.Contains(regionInfo.ISOCurrencySymbol);
+        }
+    }
+}
---
+++ src/Infrastructure/Cms/ExpressStore.Cms.Infrastructure/Helpers/ReflectionHelper.cs
@@ -0,0 +1,31 @@
+namespace ExpressStore.Cms.Infrastructure.Helpers
+{
+    public static class ReflectionHelper
+    {
+        public static bool IsAssignableToGenericType(Type givenType, Type genericType)
+        {
+            var givenTypeInfo = givenType.GetTypeInfo();
+
+            if (givenTypeInfo.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+            {
+                return true;
+            }
+
+            foreach (var interfaceType in givenTypeInfo.GetInterfaces())
+            {
+                if (interfaceType.GetTypeInfo().IsGenericType && interfaceType.GetGenericTypeDefinition() == genericType)
+                {
+                    return true;
+                }
+            }
+
+            if (givenTypeInfo.BaseType == null)
+            {
+                return false;
+            }
+
+            return IsAssignableToGenericType(givenTypeInfo.BaseType, genericType);
+        }
+        
+    }
+}
