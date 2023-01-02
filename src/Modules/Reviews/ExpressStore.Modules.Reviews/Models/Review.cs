0d6f63b42e27a550c4043187605c7f9e56fd8a32
AphonsusSchimidt <lesterlucasit@hotmail.com>
2023-01-01 18:16:46 -0800

    fet(#48): Building Reviews solution structure

---
+++ src/Modules/Reviews/ExpressStore.Modules.Reviews/Models/Review.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpressStore.Reviews.Infrastructure.Models;
+
+namespace ExpressStore.Modules.Reviews.Models
+{
+    public class Review : EntityBase
+    {
+    }
+}
