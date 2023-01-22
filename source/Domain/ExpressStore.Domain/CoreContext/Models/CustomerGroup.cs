da80ff3396c54905efbe495f04ea1661e7e0388a
LesterCerioli <lesterlucasit@hotmail.com>
2023-01-22 18:12:17 -0300

    fet(#65): Adjusting Core entities

--- source/Domain/ExpressStore.Domain/CoreContext/Models/CustomerGroup.cs
+++ source/Domain/ExpressStore.Domain/CoreContext/Models/CustomerGroup.cs
@@ -9,5 +9,27 @@ namespace ExpressStore.Domain.CoreContext.Models
 {
     public class CustomerGroup : Entity, IAggregateRoot
     {
+
+        public CustomerGroup(Guid id)
+        {
+            Id = id;
+            CreatedOn = DateTimeOffset.Now;
+            LatestUpdatedOn = DateTimeOffset.Now;
+        }
+
+        protected CustomerGroup() {}
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTimeOffset CreatedOn { get; set; }
+
+        public DateTimeOffset LatestUpdatedOn { get; set; }
+
+        public IList<CustomerGroupUser> Users { get; set; } = new List<CustomerGroupUser>();
     }
 }
